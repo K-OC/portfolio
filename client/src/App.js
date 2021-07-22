d7074d245e287f7d92c60e01010770e5ce80c972
Kieran O'Connell <kieranpoconnell@gmail.com>
2021-07-22 15:04:37 -0230

    routerdom, initial component, styled-components

--- client/src/App.js
+++ client/src/App.js
@@ -1,5 +1,27 @@
+import React from "react";
+import styled from "styled-components";
+import { BrowserRouter, Switch, Route } from "react-router-dom";
+import Home from "./Components/Home";
 function App() {
-  return <div className="App">Hello</div>;
+  return (
+    <>
+      <BrowserRouter>
+        <Wrapper>
+          <Switch>
+            <Route path="/" exact>
+              <Home />
+            </Route>
+          </Switch>
+        </Wrapper>
+      </BrowserRouter>
+    </>
+  );
 }
+
+const Wrapper = styled.div`
+  display: flex;
+  width: 100vw;
+  height: 100vh;
+`;
 
 export default App;

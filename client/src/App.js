9a0e36f2d095e65cd777e11786d7464101ec995d
Kieran O'Connell <kieranpoconnell@gmail.com>
2021-07-22 15:13:44 -0230

    organizing page

--- client/src/App.js
+++ client/src/App.js
@@ -22,6 +22,7 @@ function App() {
 
 const Wrapper = styled.div`
   display: flex;
+  flex-direction: column;
   width: 100vw;
   height: 100vh;
 `;
--- client/src/Components/Header.js
+++ client/src/Components/Header.js
@@ -1,7 +1,16 @@
 import React from "react";
-
+import styled from "styled-components";
+import { Link } from "react-router-dom";
 const Header = () => {
-  return <div></div>;
+  return (
+    <>
+      <Wrapper>HEader</Wrapper>
+    </>
+  );
 };
+
+const Wrapper = styled.div`
+  width: 100vw;
+`;
 
 export default Header;

796da79c31f6506b711d29dade4f21a7dcef68e4
Kieran O'Connell <kieranpoconnell@gmail.com>
2021-07-25 12:27:36 -0230

    styling

--- client/src/Components/Projects.js
+++ client/src/Components/Projects.js
@@ -1,5 +1,5 @@
 import React from "react";
-import styled from "styled-components"
+import styled from "styled-components";
 import Carousel from "react-multi-carousel";
 import "react-multi-carousel/lib/styles.css";
 
@@ -7,46 +7,41 @@ const Projects = () => {
   const responsive = {
     desktop: {
       breakpoint: { max: 3000, min: 1024 },
-      items: 3
+      items: 3,
     },
     tablet: {
       breakpoint: { max: 1024, min: 464 },
-      items: 2
+      items: 2,
     },
     mobile: {
       breakpoint: { max: 464, min: 0 },
-      items: 1
-    }
+      items: 1,
+    },
   };
 
   return (
     <>
-    <CardWrapper>
-    <Carousel responsive={responsive}>
-  <CardItem>Item 1</CardItem>
-  <CardItem>Item 2</CardItem>
-  <CardItem>Item 3</CardItem>
-    </Carousel>
-    </CardWrapper>
+      <Carousel responsive={responsive}>
+        <CardItem>Item 1</CardItem>
+        <CardItem>Item 2</CardItem>
+        <CardItem>Item 3</CardItem>
+      </Carousel>
     </>
-  )
+  );
 };
 
 const CardWrapper = styled.div`
-display: flex;
-align-items: center;
-color: white;
-width: 50vw;
-height: 50vh;
-
-`
+  display: flex;
+  align-items: center;
+  height: fit-content;
+  max-width: fit-content;
+  color: white;
+`;
 const CardItem = styled.div`
-width: fit-content;
-height: fit-content;
-border: 1px solid white;
-
-@media (max-width: 769px) {
+  border: 1px solid white;
+  color: white;
+  @media (max-width: 769px) {
     border: none;
   }
-`
+`;
 export default Projects;

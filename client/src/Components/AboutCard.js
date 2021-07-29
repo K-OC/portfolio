394a09cf4ed485f5a4848c25b9b35e6de3c05ae4
Kieran O'Connell <kieranpoconnell@gmail.com>
2021-07-29 13:26:29 -0230

    styling

--- client/src/Components/AboutCard.js
+++ client/src/Components/AboutCard.js
@@ -8,7 +8,7 @@ const AboutCard = () => {
         <h2>About me</h2>
         <p>
         Hi there and welcome to my portfolio! I am a full-stack developer from St. John's, Newfoundland, Canada. Having graduated from Concordia's Web Development boot camp in May, 2021, I am excited to learn and grow as a developer. 
-        Prior to shifting to development, I worked as a Director at a mid-sized non-profit. Although it was not part of my job description, I felt strongly that access to entertainment and technology was paramount in helping people find degrees of stability and safety that many of us take for granted. In my spare time, I want to apply my skills in development in ways that can benefir vulnerable and margninalized peopley in my community.
+        Prior to shifting to development, I worked as a Director at a mid-sized non-profit. Although it was not part of my job description, I felt strongly that access to entertainment and technology was paramount in helping people find degrees of stability and safety that many of us take for granted. In my spare time, I want to apply my skills in development in ways that can benefit vulnerable and margninalized peopley in my community.
         </p>
       </Wrapper>
     </>
@@ -16,10 +16,13 @@ const AboutCard = () => {
 };
 
 const Wrapper = styled.div`
+display: flex;
+flex-direction: column;
+justify-content: center;
   width: 40vw;
-  min-height: 55vh;
+  min-height: fit-content;
   padding: 1rem;
-  height: 20vw;
+  height: 50vh;
   color: #e6e6e6;
   font-family: var(--header-font);
   @media (min-width: 768px) {
--- client/src/Components/Home.js
+++ client/src/Components/Home.js
@@ -11,12 +11,10 @@ const Home = () => {
         <ImgWrapper>
           <Img src="/img.jpg" />
         </ImgWrapper>
-        <CardWrapper>
         <AboutWrapper>
           <AboutCard />
         </AboutWrapper>
         
-        </CardWrapper>
       </Wrapper>
     </>
   );
@@ -27,29 +25,25 @@ const Wrapper = styled.div`
   width: fit-content;
   min-height: fit-content;
   align-items: center;
+  justify-content: space-evenly;
 `;
 const ImgWrapper = styled.div`
-  width: 50vw;
-  height: 100vh;
+  max-width: 50vw;
+  max-height: 50vh;
+  padding-right: 2rem;
+  
 `;
 
 const Img = styled.img`
-  max-width: 40vw;
+  width: 50vw;
 `;
 
 const AboutWrapper = styled.div`
-  height: 50vh;
-  
-`;
-const ProjectsWrapper = styled.div`
-height: 50vh;
-align-items: center;
-justify-content: center;
+  height: 100vh;
+display: flex;
 `;
 
-const CardWrapper = styled.div`
-display: flex;
-flex-direction: column;
-`
+
+
 
 export default Home;

5aaf0ceefba8bf5080ba843382023e3179b474a2
Kieran O'Connell <kieranpoconnell@gmail.com>
2021-07-29 10:04:36 -0230

    add copy

--- client/src/Components/AboutCard.js
+++ client/src/Components/AboutCard.js
@@ -7,11 +7,10 @@ const AboutCard = () => {
       <Wrapper>
         <h2>About me</h2>
         <p>
-          At vero eos et accusamus et iusto odio dignissimos ducimus qui
-          blanditiis praesentium voluptatum deleniti atque corrupti quos dolores
-          et quas molestias excepturi sint occaecati cupiditate non provident,
-          similique sunt in culpa qui officia deserunt mollitia animi, id est
-          laborum et dolorum fuga.
+        Hi there and welcome to my portfolio! I am a full-stack developer from St. John's, Newfoundland, Canada. Having graduated from Concordia's Web Development boot camp in May, 2021, I am excited to learn and grow as a developer. 
+        Prior to shifting to development, I worked as a Director at a mid-sized non-profit. Although it was not part of my job description, I felt strongly that access to entertainment and technology was paramount in helping people find degrees of stability and safety that many of us take for granted. In my spare time, I want to apply my skills in development in ways that can benefir vulnerable and margninalized peopley in my community.
+
+        
         </p>
       </Wrapper>
     </>

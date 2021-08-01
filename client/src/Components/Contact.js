d5c68a0cda7cc99b8ba362418cfefc6ee7a0b606
Kieran O'Connell <kieranpoconnell@gmail.com>
2021-08-01 19:03:43 -0230

    contact component

---
+++ client/src/Components/Contact.js
@@ -0,0 +1,12 @@
+import React from "react"
+import styled from "styled-components"
+
+const Contact  = () => {
+    return (
+        <>
+        <div>hello</div>
+        </>
+    )
+}
+
+export default Contact

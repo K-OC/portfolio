553bef4d33570e9221031ab24fee45937a90a15c
Kieran O'Connell <kieranpoconnell@gmail.com>
2021-08-05 21:18:15 -0230

    title added

--- client/src/Components/Contact.js
+++ client/src/Components/Contact.js
@@ -1,18 +1,20 @@
-import React from "react"
-import styled from "styled-components"
+import React from "react";
+import styled from "styled-components";
 
-const Contact  = () => {
-    return (
-        <>
-        <Wrapper>hello</Wrapper>
-        </>
-    )
-}
+const Contact = () => {
+  return (
+    <>
+      <Wrapper>
+        <h1>Get in touch</h1>
+      </Wrapper>
+    </>
+  );
+};
 
 const Wrapper = styled.div`
-display: flex;
-height: 100vh;
-width: 100vw;
-`
+  display: flex;
+  height: 100vh;
+  width: 100vw;
+`;
 
-export default Contact
+export default Contact;

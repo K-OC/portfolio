9c94ea0880a7a19f8c02db6cf41e2e2f98b1e897
Kieran O'Connell <kieranpoconnell@gmail.com>
2021-08-05 21:30:40 -0230

    styling

--- client/src/Components/Contact.js
+++ client/src/Components/Contact.js
@@ -5,7 +5,8 @@ const Contact = () => {
   return (
     <>
       <Wrapper>
-        <h1>Get in touch</h1>
+        <StyledTitle>Get in touch</StyledTitle>
+        <StyledLink href="mailto:owner@kieran-oconnell.ca">Email</StyledLink>
       </Wrapper>
     </>
   );
@@ -13,10 +14,26 @@ const Contact = () => {
 
 const Wrapper = styled.div`
   display: flex;
+  flex-direction: column;
   align-items: center;
   justify-content: center;
   height: 100vh;
   width: 100vw;
+`;
+
+const StyledTitle = styled.h1`
+  color: #e6e6e6;
+  font-family: var(--header-font);
+`;
+
+const StyledLink = styled.a`
+  color: #e6e6e6;
+  font-family: var(--header-font);
+  text-decoration: none;
+  &:hover {
+    color: pink;
+    translate: 2px;
+  }
 `;
 
 export default Contact;

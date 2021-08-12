f9686b6fb0fb793389f5950c45b22760700c263c
Kieran O'Connell <kieranpoconnell@gmail.com>
2021-08-12 01:14:21 -0230

    add link

--- client/src/Components/Links.js
+++ client/src/Components/Links.js
@@ -7,6 +7,10 @@ const Links = () =>{
         <Wrapper>
             <h3>Links</h3>
         <StyledLink href="https://georgenervous.bandcamp.com">George Nervous Four</StyledLink>
+        <StyledLink href="https://open.spotify.com/artist/6tu0poYD76wbVyQuN8Tclw">Mild Manors</StyledLink>
+        <StyledLink href="https://www.saltwire.com/newfoundland-labrador/news/transport-subsidies-needed-for-newfoundland-and-labradors-low-income-earners-rogers-228647/">Transportation Advocacy</StyledLink>
+        <StyledLink href="https://www.cbc.ca/news/canada/newfoundland-labrador/leonard-phair-emergency-shelter-st-johns-gerry-rogers-al-hawkins-1.4684369">Housing Advocacy</StyledLink>
+        <StyledLink href="https://fb.watch/7kjZlYhiRL/">Speaking Engagement</StyledLink>
 
         </Wrapper>
         </>

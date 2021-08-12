adddfc9d87b1fc3c001ad10e27cce491ee57a449
Kieran O'Connell <kieranpoconnell@gmail.com>
2021-08-12 00:38:58 -0230

    styling

--- client/src/Components/Links.js
+++ client/src/Components/Links.js
@@ -5,6 +5,7 @@ const Links = () =>{
     return (
         <>
         <Wrapper>
+            <h3>Links</h3>
         <StyledLink href="https://georgenervous.bandcamp.com">George Nervous Four</StyledLink>
 
         </Wrapper>
@@ -15,11 +16,17 @@ const Links = () =>{
 const Wrapper = styled.div`
 display: flex;
 flex-direction: column;
+font-family: var(--header-font);
 background-color: white;
 height: 100vh;
 width: 100vw;
 `
 const StyledLink = styled.a`
 color: blue;
+text-decoration: none;
+&:visited{
+    text-decoration: underline;
+    color: purple;
+}
 `
 export default Links

ca65204cac810c495e25c8dcbf60931016203c49
Kieran O'Connell <kieranpoconnell@gmail.com>
2021-08-12 01:24:54 -0230

    styling

--- client/src/Components/Links.js
+++ client/src/Components/Links.js
@@ -6,14 +6,13 @@ const Links = () =>{
         <>
         <Wrapper>
             <h2>Links</h2>
-            <h3>Music</h3>
+            <h3>music</h3>
         <StyledLink href="https://georgenervous.bandcamp.com">George Nervous Four</StyledLink>
         <StyledLink href="https://open.spotify.com/artist/6tu0poYD76wbVyQuN8Tclw">Mild Manors</StyledLink>
-        <h3>Advocacy</h3>
+        <h3>advocacy</h3>
         <StyledLink href="https://www.saltwire.com/newfoundland-labrador/news/transport-subsidies-needed-for-newfoundland-and-labradors-low-income-earners-rogers-228647/">Transportation Advocacy</StyledLink>
         <StyledLink href="https://www.cbc.ca/news/canada/newfoundland-labrador/leonard-phair-emergency-shelter-st-johns-gerry-rogers-al-hawkins-1.4684369">Housing Advocacy</StyledLink>
         <StyledLink href="https://fb.watch/7kjZlYhiRL/">Speaking Engagement</StyledLink>
-
         </Wrapper>
         </>
     )

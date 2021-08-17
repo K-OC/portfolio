973333117967845cd349132257dd7439f77d9b9c
Kieran O'Connell <kieranpoconnell@gmail.com>
2021-08-16 23:13:51 -0230

    removed unused code

--- client/src/App.js
+++ client/src/App.js
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import React from "react";
 import styled from "styled-components";
 import { BrowserRouter, Switch, Route } from "react-router-dom";
 import Home from "./Components/Home";

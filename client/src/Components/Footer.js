0ff6cee773db110f7fe94acedadfb666f46837aa
Kieran O'Connell <kieranpoconnell@gmail.com>
2021-08-16 23:24:26 -0230

    whitespace under footer

--- client/src/Components/Footer.js
+++ client/src/Components/Footer.js
@@ -1,38 +1,38 @@
 import React from "react";
-import styled from "styled-components"
-import {Link} from "react-router-dom";
+import styled from "styled-components";
+import { Link } from "react-router-dom";
 
 const Footer = () => {
   return (
     <>
-    <FooterWrapper>
-    <StyledLink to="/contact">Contact</StyledLink>
-    <StyledLink to="/links">Links</StyledLink>
-    </FooterWrapper>
+      <FooterWrapper>
+        <StyledLink to="/contact">Contact</StyledLink>
+        <StyledLink to="/links">Links</StyledLink>
+      </FooterWrapper>
     </>
   );
 };
 
 const FooterWrapper = styled.div`
-display: flex;
-align-items: center;
-justify-content: center;
-height: 1.9rem;
-gap:13px;
-width: 100vw;
-line-height: 1;
-padding: 0;
-margin: 0;
-`
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  height: 5rem;
+  gap: 13px;
+  width: 100vw;
+  line-height: 1;
+  padding: 0;
+  margin: 0;
+`;
 
 const StyledLink = styled(Link)`
-color: white;
-text-decoration: none;
-@media (min-width: 768px) {
+  color: white;
+  text-decoration: none;
+  @media (min-width: 768px) {
     font-size: 20px;
   }
   @media (max-width: 400px) {
     font-size: 15px;
   }
-`
+`;
 export default Footer;
